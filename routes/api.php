09016a8fd80ea3d44d1b3949218fc84b69afa931
Many <you@example.comtrymany84@gmail.com>
2021-06-26 00:08:51 +0700

    assignment

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ToDoController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+//punlic
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/todo', [TodoController::class, 'index']);
+//private
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/todo', [TodoController::class, 'store']);
+    Route::put('/todo/{id}', [TodoController::class, 'update']);
+    Route::delete('/todo/{id}', [TodoController::class, 'destory']);
+});
+
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
